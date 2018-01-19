a949e29bd85c9429ef08943cb11a04bfa57dc6f4
Mikhail Bartashevich <mihailbartashevich@gmail.com>
2018-01-19 13:30:59 +0100

    yobit

--- src/app/login/login-page/login-page.component.ts
+++ src/app/login/login-page/login-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { User, UserName } from '../../shared/user.model';
-import { AuthService } from '../../shared/auth.service';
+import { AuthService, YobitTicker } from '../../shared/auth.service';
 import { LoaderBlockService } from '../../shared/loader-block.service';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
 import { takeUntil } from 'rxjs/operators';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+// import { switchMap } from 'rxjs/operators/switchMap';
 
 @Component({
   selector: 'app-login-page',
@@ -12,7 +14,7 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./login-page.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent implements OnDestroy {
+export class LoginPageComponent implements OnDestroy, OnInit {
   public user: User = new User('', '', new UserName('', ''));
   public subject: Subject<any> = new Subject();
 
@@ -21,6 +23,28 @@ export class LoginPageComponent implements OnDestroy {
     private loaderBlockService: LoaderBlockService,
     private router: Router
   ) {}
+
+       // .pipe(
+      //   map(
+      //     (info: YobitInfo) => Object.keys(info.pairs)
+      //       .filter((pair: string) => pair.indexOf('_btc') > -1)
+      //       .slice(0, 20)
+      //       .join('-')
+      //     ),
+      //   switchMap((pairs: string) => this.authService.getYobitTicker(pairs)),
+      // )
+  public ngOnInit() {
+    const pairs = this.authService.getYobitInfo().slice(0, 50);
+    this.authService.getYobitTicker(pairs.join('-'))
+      .subscribe((tickers: any) => {
+        for (const pair of pairs) {
+          const ticker: YobitTicker = tickers[pair];
+          if (ticker.low / ticker.high > 0.1 && ticker.vol > 2) {
+            console.log(pair);
+          }
+        }
+      });
+  }
 
   public onFormSubmit() {
     this.loaderBlockService.show();
--- src/app/shared/auth.http.interceptor.ts
+++ src/app/shared/auth.http.interceptor.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('auth_token') || '';
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', token)
-    });
+    const headers = request.headers;
+    if (token) {
+      headers.set('Authorization', token);
+    }
+    headers.set('Access-Control-Allow-Origin', '*');
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    headers.set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+
+    const authReq = request.clone({headers});
     return next.handle(authReq);
   }
 }
